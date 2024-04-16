4f15e2e4d7a2af85d3928e0afbb09dd36405c908
Franco Linares <franco.linares@gmail.com>
2024-04-16 15:14:22 -0300

    feat: add current goal fetch

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -1,11 +1,19 @@
-import { BASE_URL } from '../constants';
-import { Request } from '../types';
+import { BASE_URL, CANDIDATE_ID } from '../constants';
+import { HttpMethods } from '../types';
 
 export const fetchData = async <T>(
   path: string,
-  request: Request
+  request: RequestInit
 ): Promise<T> => {
   const response = await fetch(`${BASE_URL}/${path}`, request);
 
   return await response.json();
+};
+
+export const fetchCurrentGoal = async () => {
+  const response = await fetchData<any>(`map/${CANDIDATE_ID}/goal`, {
+    method: HttpMethods.GET,
+  });
+
+  return response;
 };
